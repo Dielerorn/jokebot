6f56764e1870841f6bc9870cb6cef05f7c866639
Austin <kingzap@larson-llc.com>
2018-09-11 08:29:50 -0600

    New commands

--- jokebot.rb
+++ jokebot.rb
@@ -19,6 +19,8 @@ Type `!commands` or `!help` for a list of the commands
 `!nice`
 `!ouch`
 `!doit`
+`!missionfailed`
+`!howrude`
 `!oof`
 
 **Responses**
@@ -38,6 +40,7 @@ Type `!commands` or `!help` for a list of the commands
 **Misc**
 `!howtoplaystarcraft`
 `!howtogetredditkarma`
+`!tragedy`
 `!blackpeople`
 
 **Dev Tools**
@@ -94,6 +97,11 @@ end
 bot.command :whatdidyousay do |event|
   event.respond "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo."
   puts "Navy Seal".yellow
+end
+
+bot.command :tragedy do |event|
+  event.respond "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
+  puts "Did you ever hear the tragedy of Darth Plagueis The Wise?".red
 end
 
 bot.command :howtoplaystarcraft do |event|
@@ -199,6 +207,24 @@ bot.command :oof do |event|
   puts "oof".green
   bot.voice_connect(event.user.voice_channel)
   event.voice.play_file('media/audio/oof.mp3')
+  #Replace these with your own Server ID's
+  bot.voice_destroy(coloradoCasuals)
+  bot.voice_destroy(testServer)
+end
+
+bot.command :missionfailed do |event|
+  puts "We'll Get Em Next Time".green
+  bot.voice_connect(event.user.voice_channel)
+  event.voice.play_file('media/audio/missionfailed.mp3')
+  #Replace these with your own Server ID's
+  bot.voice_destroy(coloradoCasuals)
+  bot.voice_destroy(testServer)
+end
+
+bot.command :howrude do |event|
+  puts "How Rude".blue
+  bot.voice_connect(event.user.voice_channel)
+  event.voice.play_file('media/audio/howrude.mp3')
   #Replace these with your own Server ID's
   bot.voice_destroy(coloradoCasuals)
   bot.voice_destroy(testServer)
