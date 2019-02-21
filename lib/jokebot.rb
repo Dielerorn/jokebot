664994cf7b39427c8bc5358e655780b83cc8501a
Austin <kingzap@larson-llc.com>
2019-02-21 12:45:28 -0700

    HUGE improvments to the deepfry command

--- lib/jokebot.rb
+++ lib/jokebot.rb
@@ -50,6 +50,9 @@ tastefullyRacistCommands = [:tastefullyracist, :tr]
 #Change the 2nd number in parentheses for how many files there are
 pranked = (1..9).map { |n| "../data/media/audio/pranked/#{n}.mp3" }
 
+#Change the 2nd number in parentheses for how many files there are
+emojis = (1..15).map { |n| "../data/media/emojis/#{n}.png" }
+
 #Letter replacements for hacker text
 replacements = {
   'A' => '4', 'a' => '4', 'E' => '3', 'e' => '3', 'G' => '6', 'g' => '6', 'L' => '1', 'l' => '1', 'O' => '0', 'o' => '0', 'S' => '5', 's' => '5', 'T' => '7', 't' => '7', 'I' => '!', 'i' => '!'}
@@ -137,29 +140,41 @@ end
 bot.command :deepfry do |event|
   Discordrb::LOGGER.info("I deep fried the most recent image")
   #Find the recent image by checking the channel history and filtering it
-  recent_messages = event.channel.history(15)
+  recent_messages = event.channel.history(15) #Only search the last 15 messages in the channel
   image_message = recent_messages.find { |m| m.attachments.any?(&:image?) }
   attachment = image_message.attachments.find(&:image?)
   #Download the image
   download = open(attachment.url)
   IO.copy_stream(download, '../data/media/temp/deepfry.jpg')
   #Deepfry the image
+  processing_message = event.send_message("Processing...")
   image = MiniMagick::Image.new("../data/media/temp/deepfry.jpg")
+  image_width = image.dimensions[0]
+  image_height = image.dimensions[1]
+  rand(1..4).times do #This bLock composites the emojis, randomizing between 1-4 emojis added
+    chosen_emoji = MiniMagick::Image.open("#{emojis.sample}")
+    chosen_emoji_scaled = chosen_emoji.resize "#{image_width / rand(4..8)}x#{image_height / rand(4..8)}"
+    chosen_emoji_scaled.write "../data/media/temp/emoji.png"
+    result = image.composite(MiniMagick::Image.new("../data/media/temp/emoji.png")) do |c|
+      c.compose "Over"
+      c.geometry "+#{image.dimensions[0] / rand(1..8)}+#{image.dimensions[1] / rand(1..8)}"
+    end
+    result.write "../data/media/temp/deepfry.jpg"
+  end
   image.combine_options do |b|
-    b.quality "14"
-    b.distort("shepards", "#{rand(1..200)},#{rand(1..200)} #{rand(1..200)},#{rand(1..200)} #{rand(1..200)},#{rand(1..200)} #{rand(1..200)},#{rand(1..200)}")
-    b.contrast
-    b.contrast
-    b.contrast
-    b.contrast
-    b.contrast
+    b.quality "5"
+    rand(4..16).times { b.contrast }
+    4.times {b.noise.+ "Gaussian"}
     b.colorize "15,0,0"
-    b.brightness_contrast "-12"
-    b.sharpen "80"
+    b.brightness_contrast "-6"
+    b.distort("shepards", "#{rand(1..250)},#{rand(1..250)} #{rand(1..250)},#{rand(1..250)} #{rand(1..250)},#{rand(1..250)} #{rand(1..250)},#{rand(1..250)}")
   end
   #Send the final version and delete the file
+  processing_message.delete
   event.attach_file(File.open('../data/media/temp/deepfry.jpg'))
   File.delete("../data/media/temp/deepfry.jpg")
+  File.delete("../data/media/temp/emoji.png")
+  nil
 end
 
 bot.command :thanks do |event|
