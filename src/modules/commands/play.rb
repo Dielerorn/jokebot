bf4f65823c75e8070b2c40d071b7ff92d467550e
Austin Larson <kingzap@larson-llc.com>
2020-05-12 20:26:35 -0500

    WE BACK BOIS

--- src/modules/commands/play.rb
+++ src/modules/commands/play.rb
@@ -4,7 +4,7 @@ module Bot::DiscordCommands
   module Play
     extend Discordrb::Commands::CommandContainer
     command :play do |event, link|
-      song_path = 'data/media/music/song.mp3'
+      song_path = 'data/media/music/song.ogg'
       if event.user.voice_channel == nil
         event.respond $voice_channel_error
       else
@@ -20,29 +20,14 @@ module Bot::DiscordCommands
         begin
           Discordrb::LOGGER.info("Downloading... #{link}")
           downloadingMessage = event.send_message("Downloading...")
-          YoutubeDL.get "#{link}", playlist: false, extract_audio: true, audio_format: 'mp3', format: :bestaudio,  output: song_path
+          command = %(sudo youtube-dl -o #{song_path} --extract-audio --audio-format vorbis #{link})
+          puts command
+          system(command)
           downloadingMessage.delete
         rescue
           downloadingMessage.delete
           event.respond "There was an error downloading the song"
         end
-        #Get audio data
-        #mediaInfo = MediaInfo.from(song_path)
-        #songLength = mediaInfo.audio.duration.to_i / 1000 #Song Length in seconds
-        #songLengthMinutes = [songLength / 3600, songLength / 60 % 60, songLength % 60].map { |t| t.to_s.rjust(2,'0') }.join(':') #Convert seconds into hours:minutes:seconds format
-        #Discordrb::LOGGER.info("Song is #{songLength} seconds long")
-        #Discordrb::LOGGER.info("Song is #{songLengthMinutes} minutes long")
-        #Progress Bar
-        #progressbar = ProgressBar.create(:title => "Playing in #{channel.name}   00:00 ", :starting_at => 0, :total => songLength, :remainder_mark => "-", :progress_mark => "#", :length => 140)
-        #playingMessage = event.send_message(":loud_sound: #{progressbar} #{songLengthMinutes}")
-        #Thread.new do
-          #while $currently_playing == true do
-            #sleep 7 #Sleep to prevent rate limiting on the Discord API
-            #7.times { progressbar.increment } #Increment the progress bar (7 times because it sleeps for 7 seconds)
-            #playingMessage.edit ":loud_sound: #{progressbar} #{songLengthMinutes}"
-          #end
-        #end
-        #End of Progress Bar
         #Play Music
         $currently_playing = true
         Discordrb::LOGGER.info("playing #{link}")
